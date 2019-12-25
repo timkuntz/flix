6bcfd5a8fea62f92892d24a0752e2e1fdb88cb74
Tim Kuntz <tkuntz@groupon.com>
2019-12-25 09:11:16 -0600

    Update index to show only released movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  def self.released
+    where("released_on < ?", Time.now).order(released_on: :desc)
+  end
   def flop?
     total_gross && total_gross < 325_000_000
   end
